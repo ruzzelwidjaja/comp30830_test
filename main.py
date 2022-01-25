12c1dd997a66989547ed3e026cc8beda99976fef
ruzzelwidjaja <ruzzelwidjaja@gmail.com>
2022-01-25 10:35:53 +0000

    commit on 'main'

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def main():
-    print("hello world!")
+import socket
+import os
+import platform
+import multiprocessing
+import psutil
 
-if __name__ == '__main__':
-    main()
+print('Name of machine: ',socket.gethostname())
+print('Operating SystemL ', platform.system())
+print('Operating System Version:', platform.release())
+print('Number of CPUs: ', multiprocessing.cpu_count())
+print('Amount of memory: ', psutil.virtual_memory().total)
+print('RAM memory used(%): ', psutil.virtual_memory()[2])
+print('IP address of machine ', socket.gethostbyname(socket.gethostname()))
